1ca4ad487776c82ebec44d3b8932cf1e461566bf
Josh Marsden <joshuamarsden@gmail.com>
2023-11-21 20:44:38 -0500

    Adjusted where UI code is placed and where it gets built. Cleaned up some things and fixed the Angular main component rendering.

---
+++ src/ui/app/lubna-fatima/lubna-fatima.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lubna-fatima',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lubna-fatima.component.html',
+  styleUrl: './lubna-fatima.component.scss'
+})
+export class LubnaFatimaComponent {
+
+}

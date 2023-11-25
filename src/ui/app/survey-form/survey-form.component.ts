6c84a1234bfe19fa483c307ecb76af65386b5719
Lubnafa <lfatima@gmu.edu>
2023-11-24 20:21:25 -0500

    commit

--- src/ui/app/survey-form/survey-form.component.ts
+++ src/ui/app/survey-form/survey-form.component.ts
@@ -1,27 +1,101 @@
-/**
- * Contributors:
- * - Josh Marsden
- * - Lubna Fatima
- */
-
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { ReactiveFormsModule,FormBuilder, FormGroup,Validators,FormArray,FormControl,AbstractControl } from '@angular/forms'; 
 import { SurveyResponsesService } from '../service/survey-responses.service';
+import {  SurveyResponse } from '../service/survey-responses.service';
 
 @Component({
   selector: 'app-survey-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule // Add ReactiveFormsModule here
+  ],
   templateUrl: './survey-form.component.html',
-  styleUrl: './survey-form.component.scss'
+  styleUrls: ['./survey-form.component.scss']
 })
-export class SurveyFormComponent {
-  responseData = this.survey.getAllResponses();
+export class SurveyFormComponent implements OnInit {
+  surveyForm!: FormGroup; // Declare the surveyForm property here
+  showSuccess: boolean = false; 
+submit()
+{
+  console.log("Login success");
+}
+constructor(private fb: FormBuilder, private surveyService: SurveyResponsesService) { }
 
-  constructor (private survey: SurveyResponsesService) { }
+ResponseData!: Observable<SurveyResponse[]>;
+
+
+  //constructor(private surveyService: SurveyResponsesService) { }
+  validateError(controlName: string): boolean {
+    const control = this.surveyForm.get(controlName);
+    return control ? control.invalid && control.touched : false;
+  }
+  likesAbt: { name: string; value: string }[] = [
+    { name: 'Students', value: 'students' },
+    { name: 'Location', value: 'location' },
+    // ... other options ...
+  ];
+  hearAbt: { name: string; value: string }[] = [
+    { name: 'Students', value: 'students' },
+    { name: 'Location', value: 'location' },
+    // ... other options ...
+  ];
+  recommend: { name: string; value: string }[]=[
+    { name: 'Very Likely', value: 'veryLikely' },
+    { name: 'Likely', value: 'likely' },
+    { name: 'Unlikely', value: 'unlikely' }
+    // Add other options as needed
+  ];
+  onCheckBoxChange(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    const likesArray = this.surveyForm.get('likesArray') as FormArray;
+  
+    if (checkbox.checked) {
+      likesArray.push(this.fb.control(checkbox.value));
+    } else {
+      let i = 0;
+      likesArray.controls.forEach((ctrl: AbstractControl) => {
+        if (ctrl.value == checkbox.value) {
+          likesArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+  
+
+  // ... other methods ...
+
+
 
   ngOnInit() {
-    this.survey.addResponse({
+    this.initializeForm();
+    this.loadSurveyResponses();
+    //this.addDummyResponse(); // This should probably be removed in production
+  }
+  initializeForm() {
+    this.surveyForm = this.fb.group({
+      // ... other form controls ...
+      hearAbtCampus: ['', Validators.required], // Include hearAbtCampus here
+      // ... more form controls ...
+    });
+  }
+  loadSurveyResponses() {
+    this.ResponseData = this.surveyService.getAllResponses();
+  }
+  cancelSurvey() {
+    this.surveyForm.reset(); // Reset the form to its initial state
+  }
+  onChangeRecommend(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.surveyForm.get('recommend')?.setValue(selectedValue);
+  }
+
+  addDummyResponse() {
+    const dummyResponse = {
       firstName: 'Bob',
       lastName: 'Ross',
       streetAddress: '123 Abc Rd',
@@ -34,13 +108,15 @@ export class SurveyFormComponent {
       reference: '',
       recommendationLikelihood: 1,
       surveyDate: new Date(),
-    }).subscribe((res) => {
-      console.log({res});
-    });
+    };
 
-    this.responseData.subscribe((res) => {
-      const surveys = res.data;
-      console.log({surveys});
+    this.surveyService.addResponse(dummyResponse).subscribe({
+      next: (res) => {
+        console.log('Response added', res);
+      },
+      error: (err) => {
+        console.error('Error adding response', err);
+      }
     });
   }
 }

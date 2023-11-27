be0754541c39f8e8493e8afa8290ada291b8439b
Lubnafa <lfatima@gmu.edu>
2023-11-27 10:43:35 -0500

    created war file in target folder

---
+++ src/main/java/swe642/hw3/webserver/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package swe642.hw3.webserver.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServletInitializer.class);
+    }
+
+}
